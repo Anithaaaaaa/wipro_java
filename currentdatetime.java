7832fb3b9c01eb914008f0d3f0c2d0b153a5e009
Anithaaaaaa <167348943+Anithaaaaaa@users.noreply.github.com>
2025-07-29 15:34:50 +0530

    Add files via upload

---
+++ currentdatetime.java
@@ -0,0 +1,13 @@
+package javastring;
+import java.util.Calendar;
+
+public class currentdatetime {
+	public static void main(String[] args) {
+		Calendar c = Calendar.getInstance();
+		 System.out.println("Current Date and Time :");
+		 System.out.format("%tB %te, %tY%n", c, c, c);
+		 System.out.format("%tl:%tM %tp%n", c, c, c);
+    
+	}
+}
+		 
