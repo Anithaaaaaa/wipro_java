a93a53b0c2d1c7a1382d42a1770e71f323045c65
Anithaaaaaa <167348943+Anithaaaaaa@users.noreply.github.com>
2025-07-30 18:59:45 +0530

    Delete currentdatetime.java

--- currentdatetime.java
+++ currentdatetime.java
@@ -1,13 +1,1 @@
-package javastring;
-import java.util.Calendar;
 
-public class currentdatetime {
-	public static void main(String[] args) {
-		Calendar c = Calendar.getInstance();
-		 System.out.println("Current Date and Time :");
-		 System.out.format("%tB %te, %tY%n", c, c, c);
-		 System.out.format("%tl:%tM %tp%n", c, c, c);
-    
-	}
-}
-		 
